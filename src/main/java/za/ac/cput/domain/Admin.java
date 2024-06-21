a3b60f6612752a660c52de2f08dc3a71ef53f9cc
Joshua@mycput <222191562@mycput.ac.za>
2024-06-21 03:40:05 +0200

    Revert " Admin employeeID hanged to Long data type , removed password attibute"
    This reverts commit b27a21384b3b0bd234e21caa3e95902e1150e827.

--- src/main/java/za/ac/cput/domain/Admin.java
+++ src/main/java/za/ac/cput/domain/Admin.java
@@ -7,7 +7,9 @@ import java.util.Objects;
 @Entity
 public class Admin extends User {
     @Id
-    private Long employeeId;
+    private String employeeId;
+    private String password;
+
 
     public Admin() {
         super();
@@ -17,12 +19,15 @@ public class Admin extends User {
     public Admin(AdminBuilder builder) {
         super();
         this.employeeId = builder.employeeId;
+        this.password = builder.password;
     }
 
-    public Long getEmployeeId() {
+    public String getEmployeeId() {
         return employeeId;
     }
-
+    public String getPassword() {
+        return password;
+    }
 
 
     @Override
@@ -30,12 +35,12 @@ public class Admin extends User {
         if (this == o) return true;
         if (!(o instanceof Admin admin)) return false;
         if (!super.equals(o)) return false;
-        return Objects.equals(employeeId, admin.employeeId);
+        return Objects.equals(employeeId, admin.employeeId) && Objects.equals(password, admin.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), employeeId);
+        return Objects.hash(super.hashCode(), employeeId, password);
     }
 
     @Override
@@ -43,23 +48,30 @@ public class Admin extends User {
         return "Admin{" +
                  super.toString()+
                 "employeeId='" + employeeId + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 
 
     public static class AdminBuilder {
 
-        private Long employeeId;
+        private String employeeId;
+        private String password;
 
-        public AdminBuilder setEmployeeId(Long employeeId) {
+        public AdminBuilder setEmployeeId(String employeeId) {
             this.employeeId = employeeId;
             return this;
         }
 
+        public AdminBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
 
        public  AdminBuilder copy (Admin admin){
 
             this.employeeId = admin.employeeId;
+            this.password = admin.password;
             return this;
        }
 
